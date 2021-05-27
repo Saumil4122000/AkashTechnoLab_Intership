1bd38b316be344931b8266057b8ce4f13bb1c1cc
Saumil4122000 <73154948+Saumil4122000@users.noreply.github.com>
2021-05-28 00:39:08 +0530

    Task1 Completed, also added apk file for demo

---
+++ Task1/Java/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),OTPEnterActivity.class));
+            }
+        });
+    }
+}
---
+++ Task1/Java/OTPEnterActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPEnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_o_t_p_enter);
+    }
+}
